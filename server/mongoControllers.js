f144b96ab1a0fe3eec657891e42678432c8cf6a2
Kyle McCarty <kylemccarty@Kyles-MacBook-Air.local>
2018-07-02 19:28:34 -0700

    add delete and post routes, add get by question id route

--- server/mongoControllers.js
+++ server/mongoControllers.js
@@ -14,6 +14,20 @@ const dbExport = require('../database/db_mongo/index')
 
   //req.restaurantId
 
+const individualQController = {
+  get: (req, res) => {
+    const questionsDB = dbExport()
+    const query = Number(req.params.id)
+    questionsDB.collection('question').find({id: query }).toArray(function(err, question) {
+      if (err) {
+        console.log('error in question get', err)
+      }
+      res.send(question)
+    })
+  }
+}
+
+
 const askQsController = {
   get: (req, res) => {
     const questionsDB = dbExport()
@@ -22,7 +36,7 @@ const askQsController = {
       if (err) {
         console.log('error in question get', err)
       }
-      res.sendStatus(questions)
+      res.send(questions)
     })
   },
   put: (req, res) => {
@@ -35,7 +49,33 @@ const askQsController = {
       }
       res.sendStatus(200)
     }) 
+  },
+  delete: (req, res) => {
+    const questionsDB = dbExport()
+    const query = Number(req.params.id)
+    questionsDB.collection('question').deleteOne( { id: query }, function(err, something) {
+      if (err) {
+        console.log('error in put', err)
+        res.sendStatus(404)
+      }
+      res.sendStatus(200)
+    })
+  },
+  post: (req, res) => {
+    const questionsDB = dbExport()
+    questionsDB.collection('question').insertOne({id: Number(req.body.id), user_id: req.body.user_id, restaurant_id: req.body.restaurant_id, question: req.body.question, parent_id: req.body.parent_id, helpful: req.body.helpful, createdat: req.body.createdat, updatedat: req.body.updatedat}, function(err){
+      if (err) {
+        console.log('error in post request', err)
+        res.sendStatus(404)
+      }
+      res.sendStatus(200)
+    })
   }
 }
 
-module.exports = { askQsController }
+module.exports = { askQsController,  individualQController }
+
+// { "_id" : ObjectId("5b36c855834415ecc87337c7"), "id" : 3617, "user_id" : 22842, "restaurant_id" : 7816, 
+// "question" : "Why pairs the chicken stir?", "parent_id" : 0, "helpful" : 59, 
+// "createdat" : "Tue Sep 25 2018 00:01:29 GMT-0700 (PDT)", 
+// "updatedat" : "Tue Sep 25 2018 00:01:29 GMT-0700 (PDT)" }
--- server/mongoRoutes.js
+++ server/mongoRoutes.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const controller = require('./mongoControllers.js');
 
-// router.route('/questions')
-// .post(controller.askQsController.post)
-// .put(controller.askQsController.put)
-// .delete(controller.askQsController.delete);
+router.route('/questions')
+.post(controller.askQsController.post)
 router.route('/questions/:id')
 .get(controller.askQsController.get)
-//.delete(controller.askQsController.delete)
+.delete(controller.askQsController.delete)
 .put(controller.askQsController.put)
+router.route('/id/:id')
+.get(controller.individualQController.get)
+
+
 // router.route('/users')
 // .get(controller.todolist.fetch)
 // .post(controller.todolist.post)
